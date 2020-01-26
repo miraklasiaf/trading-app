9551f8ee5531eacc6cf2c7940e793f9a47915a67
Faisal Karim <faisalkarim96@gmail.com>
2020-01-26 18:05:11 +0700

    Remove console log

--- src/store/actions.js
+++ src/store/actions.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 export const loadData = ({ commit }) => {
 	axios.get('https://trading-app-c88cc.firebaseio.com/data.json').then((res) => {
-		console.log(res);
 		const stocks = res.data.stocks;
 		const funds = res.data.funds;
 		const stockPortfolio = res.data.stockPortfolio;
